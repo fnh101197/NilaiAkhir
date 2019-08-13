5438f2756c5e13310e6c4b55cd40e26ac9e43a7f
Fajar Nur Hidayat <fnh101197@gmail.com>
2019-08-13 22:11:06 +0700

    NilaiAkhir

---
+++ Aplikasi Nilai Akhir/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Aplikasi_Nilai_Akhir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nama = new System.Windows.Forms.ComboBox();
+            this.matkul = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.absensi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.t2 = new System.Windows.Forms.TextBox();
+            this.t3 = new System.Windows.Forms.TextBox();
+            this.t4 = new System.Windows.Forms.TextBox();
+            this.t5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.praktikum = new System.Windows.Forms.TextBox();
+            this.nilaiuts = new System.Windows.Forms.TextBox();
+            this.nilaiuas = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.nilaiakhir = new System.Windows.Forms.TextBox();
+            this.predikat = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(456, 76);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "   APLIKASI PENGHITUNG\r\nNILAI AKHIR MATA KULIAH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NAMA MAHASISWA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "MATA KULIAH";
+            // 
+            // nama
+            // 
+            this.nama.FormattingEnabled = true;
+            this.nama.Location = new System.Drawing.Point(163, 138);
+            this.nama.Name = "nama";
+            this.nama.Size = new System.Drawing.Size(234, 24);
+            this.nama.TabIndex = 3;
+            this.nama.SelectedIndexChanged += new System.EventHandler(this.nama_SelectedIndexChanged);
+            // 
+            // matkul
+            // 
+            this.matkul.FormattingEnabled = true;
+            this.matkul.Location = new System.Drawing.Point(163, 177);
+            this.matkul.Name = "matkul";
+            this.matkul.Size = new System.Drawing.Size(234, 24);
+            this.matkul.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "ABSENSI";
+            // 
+            // absensi
+            // 
+            this.absensi.Location = new System.Drawing.Point(163, 216);
+            this.absensi.Name = "absensi";
+            this.absensi.Size = new System.Drawing.Size(100, 22);
+            this.absensi.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "NILAI TUGAS";
+            // 
+            // t1
+            // 
+            this.t1.Location = new System.Drawing.Point(163, 254);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(35, 22);
+            this.t1.TabIndex = 8;
+            // 
+            // t2
+            // 
+            this.t2.Location = new System.Drawing.Point(204, 254);
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(35, 22);
+            this.t2.TabIndex = 9;
+            // 
+            // t3
+            // 
+            this.t3.Location = new System.Drawing.Point(245, 254);
+            this.t3.Name = "t3";
+            this.t3.Size = new System.Drawing.Size(35, 22);
+            this.t3.TabIndex = 10;
+            // 
+            // t4
+            // 
+            this.t4.Location = new System.Drawing.Point(286, 254);
+            this.t4.Name = "t4";
+            this.t4.Size = new System.Drawing.Size(35, 22);
+            this.t4.TabIndex = 11;
+            // 
+            // t5
+            // 
+            this.t5.Location = new System.Drawing.Point(327, 254);
+            this.t5.Name = "t5";
+            this.t5.Size = new System.Drawing.Size(35, 22);
+            this.t5.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 293);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "NILAI PRAKTIKUM";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 334);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "NILAI UTS";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 374);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 17);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "NILAI UAS";
+            // 
+            // praktikum
+            // 
+            this.praktikum.Location = new System.Drawing.Point(163, 293);
+            this.praktikum.Name = "praktikum";
+            this.praktikum.Size = new System.Drawing.Size(100, 22);
+            this.praktikum.TabIndex = 16;
+            // 
+            // nilaiuts
+            // 
+            this.nilaiuts.Location = new System.Drawing.Point(163, 334);
+            this.nilaiuts.Name = "nilaiuts";
+            this.nilaiuts.Size = new System.Drawing.Size(100, 22);
+            this.nilaiuts.TabIndex = 17;
+            // 
+            // nilaiuas
+            // 
+            this.nilaiuas.Location = new System.Drawing.Point(163, 374);
+            this.nilaiuas.Name = "nilaiuas";
+            this.nilaiuas.Size = new System.Drawing.Size(100, 22);
+            this.nilaiuas.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(507, 164);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 17);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "NILAI AKHIR";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(512, 296);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(76, 17);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "PREDIKAT";
+            // 
+            // nilaiakhir
+            // 
+            this.nilaiakhir.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nilaiakhir.Location = new System.Drawing.Point(475, 184);
+            this.nilaiakhir.Multiline = true;
+            this.nilaiakhir.Name = "nilaiakhir";
+            this.nilaiakhir.Size = new System.Drawing.Size(151, 71);
+            this.nilaiakhir.TabIndex = 21;
+            this.nilaiakhir.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+            // 
+            // predikat
+            // 
+            this.predikat.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.predikat.Location = new System.Drawing.Point(517, 316);
+            this.predikat.Multiline = true;
+            this.predikat.Name = "predikat";
+            this.predikat.Size = new System.Drawing.Size(67, 71);
+            this.predikat.TabIndex = 22;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 450);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 49);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "HITUNG";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(274, 450);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 49);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "RESET";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 581);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.predikat);
+            this.Controls.Add(this.nilaiakhir);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.nilaiuas);
+            this.Controls.Add(this.nilaiuts);
+            this.Controls.Add(this.praktikum);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.t5);
+            this.Controls.Add(this.t4);
+            this.Controls.Add(this.t3);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.absensi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.matkul);
+            this.Controls.Add(this.nama);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox nama;
+        private System.Windows.Forms.ComboBox matkul;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox absensi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.TextBox t2;
+        private System.Windows.Forms.TextBox t3;
+        private System.Windows.Forms.TextBox t4;
+        private System.Windows.Forms.TextBox t5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox praktikum;
+        private System.Windows.Forms.TextBox nilaiuts;
+        private System.Windows.Forms.TextBox nilaiuas;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox nilaiakhir;
+        private System.Windows.Forms.TextBox predikat;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Aplikasi Nilai Akhir/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplikasi_Nilai_Akhir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool isEmpety(string username, string password)
+        {
+            if (absensi.Equals("") && t1.Equals("") && t2.Equals("") && t3.Equals("") && t4.Equals("")
+            && t5.Equals("") && praktikum.Equals("") && nilaiuts.Equals("") && nilaiuas.Equals(""))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            double a = 0.0;//Deklarasi TipeData
+            double nt1 = 0.0;
+            double nt2 = 0.0;
+            double nt3 = 0.0;
+            double nt4 = 0.0;
+            double nt5 = 0.0;
+            double np = 0.0;
+            double nuts = 0.0;
+            double nuas = 0.0;
+            double akhir = 0.0;
+            string komen;
+            a = Double.Parse(absensi.Text);//Deklarasi Variable
+            nt1 = Double.Parse(t1.Text);
+            nt2 = Double.Parse(t2.Text);
+            nt3 = Double.Parse(t3.Text);
+            nt4 = Double.Parse(t4.Text);
+            nt5 = Double.Parse(t5.Text);
+            np = Double.Parse(praktikum.Text);
+            nuts = Double.Parse(nilaiuts.Text);
+            nuas = Double.Parse(nilaiuas.Text);
+            akhir = ((a * 10 / 100) + ((((nt1 + nt2 + nt3 + nt4 + nt5) * 2) / 10) * 20 / 100) +
+            (np * 25 / 100) + (nuts * 20 / 100) + (nuas * 25 / 100));
+            nilaiakhir.Text = akhir.ToString("n2");//Parameter Penghitungan Nilai Akhir
+            if (akhir >= 80)//Looping Untuk Menentukan Predikat Nilai
+                komen = "A";
+            else if (akhir >= 70)
+                komen = "B";
+            else if (akhir >= 60)
+                komen = "C";
+            else if (akhir >= 50)
+                komen = "D";
+            else
+                komen = "E";
+            predikat.Text = komen;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            absensi.Clear();//sistem penghapusan Komponen sesuai nama Komponen
+            t1.Clear();
+            t2.Clear();
+            t3.Clear();
+            t4.Clear();
+            t5.Clear();
+            praktikum.Clear();
+            nilaiuts.Clear();
+            nilaiuas.Clear();
+            nilaiakhir.Clear();
+            predikat.Clear();
+            nama.SelectedIndex = -1;
+            matkul.SelectedIndex = -1;
+        }
+
+        private void nama_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nama.Items.Add("Fajar");
+            nama.Items.Add("Nur");
+            nama.Items.Add("Hidayat");
+            nama.Items.Add("Fanha");
+            nama.Items.Add("Nurhi");
+            matkul.Items.Add("Pemrograman Web 1");
+            matkul.Items.Add("Pemrograman Visual 1");
+            matkul.Items.Add("Pemrograman Berorientasi Objek");
+            matkul.Items.Add("Analisis Perancangan Sistem");
+            matkul.Items.Add("Pendidikan Kewarganegaraan");
+            matkul.Items.Add("Sistem Informasi Manajemen");
+            matkul.Items.Add("Teknik Animasi dan Multimedia");
+            matkul.Items.Add("Jaringan Komputer");
+        }
+    }
+}
